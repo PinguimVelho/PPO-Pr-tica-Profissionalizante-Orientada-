c3ecc0fc95f0dd9cec22f0b244755b3a7b049c59
Francisco <frangeissmann@gmail.com>
2026-06-02 17:51:12 +0000

    Comecei o Site

---
+++ 02 - Sfypot/main.js
@@ -0,0 +1,38 @@
+// Local Storage
+const setLocalStorage = (musicas_db) => localStorage.setItem('musicas_db', JSON.stringify)
+const geLocalStorage = () => JSON.parse(localStorage.getItem('musicas_db')) ?? []
+
+// Classe
+
+class Musica {
+  constructor(titulo, autor, genero ) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.genero = genero;
+  }
+}
+
+// CRUD
+
+const createMusica = (musica) => {
+    const musicas_db = readMusicas()
+    musicas_db.push = musica
+    setLocalStorage(musicas_db)
+}
+
+
+const readMusicas = () => getLocalStorage()
+
+
+const updateMusica = (index, updMusica) => {
+    const musicas_db = readMusicas()
+    musicas_db[index] = updMusica
+    setLocalStorage(musicas_db)
+}
+
+
+const deleteMusica = () => {
+    const musicas_db = readMusicas()
+    musicas_db.splice(index, 1)
+    setLocalStorage(musicas_db)
+}
